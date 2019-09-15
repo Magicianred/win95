37826f802e8588eabe05d8aa1d74e4e43c7c9854
Arish-Shah <arish0246@gmail.com>
2019-09-16 01:22:41 +0530

    Updates

---
+++ precache-manifest.952e138d610521a50fa47f246df8598e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39492a5784ce03d96de8779633d0df93",
+    "url": "/win95/index.html"
+  },
+  {
+    "revision": "fdc842faed1e11d28248",
+    "url": "/win95/static/css/main.7db232a7.chunk.css"
+  },
+  {
+    "revision": "a92a5a1ba0f9a2d5d210",
+    "url": "/win95/static/js/2.0420b9dd.chunk.js"
+  },
+  {
+    "revision": "fdc842faed1e11d28248",
+    "url": "/win95/static/js/main.c4a55c87.chunk.js"
+  },
+  {
+    "revision": "e1a16012ed3795a778ac",
+    "url": "/win95/static/js/runtime~main.07cbb5a3.js"
+  },
+  {
+    "revision": "bf40df6254d9fa37eca8cbb28635b506",
+    "url": "/win95/static/media/Fixedsys.bf40df62.woff2"
+  },
+  {
+    "revision": "faea3367f031e3c407a88316ccc93c45",
+    "url": "/win95/static/media/MSSansSerif.faea3367.woff2"
+  },
+  {
+    "revision": "5eea62875443d3f0996fde5929fae727",
+    "url": "/win95/static/media/MSSansSerifBold.5eea6287.woff2"
+  },
+  {
+    "revision": "6863793f81edf99f1d8f6d062d172f56",
+    "url": "/win95/static/media/shutdown.6863793f.png"
+  },
+  {
+    "revision": "337a0b0ec9de91fcece69894f54bf610",
+    "url": "/win95/static/media/startup.337a0b0e.mp3"
+  }
+]);
